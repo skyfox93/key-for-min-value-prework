c1fd10ca3a4b19e546c735f682518a38ec5d576a
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-09 22:49:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-memory={key:"key", val:10**10}
+memory={key: , val:10**10}
 name_hash.each do |key,value|
 if value<memory[:val]
   memory={key: key, val: value}
