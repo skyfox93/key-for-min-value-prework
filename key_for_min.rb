1b0a7065c6d20ed7645f69c8771a2f33fdc741ef
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-09 22:52:39 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
 memory={key:nil , val:nil }
 name_hash.each do |key,value|
-if (!memory[key])||value<memory[:val]
+if (!memory[:key])||value<memory[:val]
   memory={key: key, val: value}
 end
 end
